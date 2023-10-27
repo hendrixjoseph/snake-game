7f47f20e51820a962a15eb58f5144e0ef97fde9e
hendrixjoseph <hendixjoseph@aol.com>
2023-10-27 16:47:56 -0400

    move snake around

--- src/ts/SnakeScene.ts
+++ src/ts/SnakeScene.ts
@@ -2,7 +2,13 @@ export default class SnakeScene extends Phaser.Scene {
   public static Name = "SnakeScene";
 
   snakeHead: Phaser.GameObjects.Rectangle;
+  snakeBody: Phaser.GameObjects.Rectangle[] = [];
   cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  xVelocity = 0;
+  yVelocity = 0;
+
+  lastUpdateTime = 0;
 
   create(): void {
     this.cursors = this.input.keyboard!.createCursorKeys();
@@ -10,13 +16,44 @@ export default class SnakeScene extends Phaser.Scene {
     this.snakeHead = this.add.rectangle(400, 300, 20, 20, 0xff0000);
   }
 
-  update() {
+  update(time: number) {
     if (this.cursors.left.isDown) {
-
+      this.xVelocity = -1;
+      this.yVelocity = 0;
     } else if (this.cursors.right.isDown) {
-
-    } else {
-
+      this.xVelocity = 1;
+      this.yVelocity = 0;
+    } else if (this.cursors.up.isDown) {
+      this.xVelocity = 0;
+      this.yVelocity = -1;
+    } else if (this.cursors.down.isDown) {
+      this.xVelocity = 0;
+      this.yVelocity = 1;
     }
+
+
+    if (this.xVelocity !== 0 || this.yVelocity !== 0) {
+      let timeDiff = time - this.lastUpdateTime;
+
+      if (timeDiff > 500) {
+        this.lastUpdateTime = time;
+
+        this.snakeBody.unshift(this.snakeHead);
+
+        if (this.snakeBody.length > 10) {
+          let deadPart = this.snakeBody.pop();
+          deadPart?.destroy();
+        }
+
+        this.snakeHead = this.add.rectangle(
+          this.snakeHead.x + this.xVelocity * this.snakeHead.width,
+          this.snakeHead.y + this.yVelocity * this.snakeHead.height,
+          this.snakeHead.width,
+          this.snakeHead.height,
+          this.snakeHead.fillColor
+        )
+      }
+    }
+
   }
 }
