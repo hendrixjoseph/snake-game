958afef2d588d1c3b9da54cf7ab608ca2e7381e3
hendrixjoseph <hendixjoseph@aol.com>
2023-10-28 16:30:27 -0400

    eat food

--- src/ts/SnakeScene.ts
+++ src/ts/SnakeScene.ts
@@ -3,7 +3,10 @@ export default class SnakeScene extends Phaser.Scene {
 
   snakeHead: Phaser.GameObjects.Rectangle;
   snakeBody: Phaser.GameObjects.Rectangle[] = [];
+
   cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+
+  food: Phaser.GameObjects.Star;
 
   xVelocity = 0;
   yVelocity = 0;
@@ -14,9 +17,34 @@ export default class SnakeScene extends Phaser.Scene {
     this.cursors = this.input.keyboard!.createCursorKeys();
 
     this.snakeHead = this.add.rectangle(400, 300, 20, 20, 0xff0000);
+
+    this.food = this.add.star(450, 350, 5, 5, 10, 0x00ff00);
+  }
+
+  eat(food: Phaser.GameObjects.Star) {
+    food.destroy();
+    
   }
 
   update(time: number) {
+    this.setDirection();
+
+    if (this.xVelocity !== 0 || this.yVelocity !== 0) {
+      let timeDiff = time - this.lastUpdateTime;
+
+      if (timeDiff > 500) {
+        this.lastUpdateTime = time;
+
+        this.move();
+
+        if (Phaser.Geom.Intersects.RectangleToRectangle(this.snakeHead.getBounds(), this.food.getBounds())) {
+          this.eat(this.food);
+        }
+      }
+    }
+  }
+
+  setDirection() {
     if (this.cursors.left.isDown) {
       this.xVelocity = -1;
       this.yVelocity = 0;
@@ -31,29 +59,22 @@ export default class SnakeScene extends Phaser.Scene {
       this.yVelocity = 1;
     }
 
+  }
 
-    if (this.xVelocity !== 0 || this.yVelocity !== 0) {
-      let timeDiff = time - this.lastUpdateTime;
+  move() {
+    this.snakeBody.unshift(this.snakeHead);
 
-      if (timeDiff > 500) {
-        this.lastUpdateTime = time;
-
-        this.snakeBody.unshift(this.snakeHead);
-
-        if (this.snakeBody.length > 10) {
-          let deadPart = this.snakeBody.pop();
-          deadPart?.destroy();
-        }
-
-        this.snakeHead = this.add.rectangle(
-          this.snakeHead.x + this.xVelocity * this.snakeHead.width,
-          this.snakeHead.y + this.yVelocity * this.snakeHead.height,
-          this.snakeHead.width,
-          this.snakeHead.height,
-          this.snakeHead.fillColor
-        )
-      }
+    if (this.snakeBody.length > 10) {
+      let deadPart = this.snakeBody.pop();
+      deadPart?.destroy();
     }
 
+    this.snakeHead = this.add.rectangle(
+      this.snakeHead.x + this.xVelocity * this.snakeHead.width,
+      this.snakeHead.y + this.yVelocity * this.snakeHead.height,
+      this.snakeHead.width,
+      this.snakeHead.height,
+      this.snakeHead.fillColor
+    )
   }
 }
