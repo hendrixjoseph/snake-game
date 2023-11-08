99d120b889dbe1c5e70455f64607f8f36a259f65
hendrixjoseph <hendixjoseph@aol.com>
2023-11-08 14:37:31 -0500

    hook up fullscreen button

--- src/ts/SnakeScene.ts
+++ src/ts/SnakeScene.ts
@@ -40,15 +40,19 @@ export default class SnakeScene extends Phaser.Scene {
       this.createFood("faster", 350, 250),
     ];
 
-    const fKey = this.input.keyboard!.addKey("F");
-
-    fKey.on("down", () => {
+    let toggleFullScreen = () => {
       if (this.scale.isFullscreen) {
         this.scale.stopFullscreen();
       } else {
         this.scale.startFullscreen();
       }
-    });
+    }
+
+    const fKey = this.input.keyboard!.addKey("F");
+    fKey.on("down", toggleFullScreen);
+
+    let fullScreenButton = document.getElementById('fullScreenButton') as HTMLElement;
+    fullScreenButton.addEventListener('click', toggleFullScreen);
   }
 
   createFood(type: Food, x: number, y: number) {
